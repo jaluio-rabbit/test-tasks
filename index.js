4af2763c52ed92de884c621a0de2525b6f8c8a48
jaluio-rabbit <111268636+jaluio-rabbit@users.noreply.github.com>
2022-10-02 13:15:50 +0000

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Test = void 0;
-var Test;
-(function (Test) {
-    Test.test = "hello, world!";
-    Test.getPass = (data) => data;
-})(Test = exports.Test || (exports.Test = {}));
-console.log(Test.getPass(0));
